332405b1daac2a98a8a7fb2314a9efe13f9f25aa
alexhwebdev@gmail.com <alexhwebdev@gmail.com>
2022-12-19 14:23:50 -0800

    UPDATE Features page

---
+++ src/pages/ibd-digital/features.js
@@ -0,0 +1,22 @@
+/* ==========  REACT  |  GATSBY  |  PLUGINS  ========== */
+import React, {
+  // useEffect
+} from "react";
+import { graphql } from 'gatsby';
+// import styled from 'styled-components';
+// import { globalHistory } from '@reach/router'
+
+
+const IBD_Overview = (props) => {
+  console.log('IBD_Overview props', props)
+
+
+  return (
+    <div className="ibd-index">
+			Features
+    </div>
+  )
+};
+
+export default IBD_Overview;
+
