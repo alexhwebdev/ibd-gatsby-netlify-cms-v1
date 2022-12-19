5307ac1426901ac5f7bb3059c5542d93292f7185
alexhwebdev@gmail.com <alexhwebdev@gmail.com>
2022-12-19 12:43:02 -0800

    UPDATE

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -22,7 +22,7 @@ export default class BlogIndexPage extends React.Component {
               padding: "1rem",
             }}
           >
-            Latest Stories
+            IBD Gatsby Netlify CMS Latest Stories
           </h1>
         </div>
         <section className="section">
